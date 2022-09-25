14e3896198a49995a2dfcc4ffca19e251b8ac194
Luiz Martins <luizmartins.prog@gmail.com>
2022-09-25 10:14:28 -0300

    feat: ensure SignUpController returns 400 if no valid email is provided

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -1,6 +1,9 @@
 import { HttpRequest, HttpResponse } from '../protocols/http'
 import { MissingParamError } from '../errors/missing-param-error'
 import { badRequest } from '../helpers/http-helper'
+import { StringValidator } from '../helpers/string-validation-helper'
+
+const stringValidator = new StringValidator()
 export class SignUpController {
     handle(httpRequest: HttpRequest): HttpResponse {
         const requiredFields = ['name', 'email']
@@ -8,6 +11,13 @@ export class SignUpController {
         for(const field of requiredFields) {
             if(!httpRequest.body[field]) {
                 return badRequest(new MissingParamError(field))
+            }
+        }
+
+        if(!stringValidator.validateEmail(httpRequest.body.email)) {
+            return {
+                statusCode: 400,
+                body: {}
             }
         }
 
