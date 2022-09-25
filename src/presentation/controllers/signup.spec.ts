a36d885ed8baebab36f2bbc7960960e24bee04e6
Luiz Martins <luizmartins.prog@gmail.com>
2022-09-25 10:14:36 -0300

    test: ensure SignUpController returns 400 if no valid email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -35,4 +35,19 @@ describe('SignUp controller', () => {
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     }) 
 
+
+    test('should return 400 if no valid email is provided', () => {
+        const sut = new SignUpController()
+        const httpRequest = {
+            body: {
+                name: 'anyName',
+                email: 'email@.com',
+                password: 'any',
+                confirmPassword: 'any'
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+    }) 
 })
