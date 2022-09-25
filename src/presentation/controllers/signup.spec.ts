ab970f969e48f2ac36d10de3db87233645d8770e
Luiz Martins <luizmartins.prog@gmail.com>
2022-09-25 10:04:19 -0300

    refactor: move StringValidator test to a specific test suite

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -3,7 +3,6 @@ import { MissingParamError } from '../errors/missing-param-error'
 import { StringValidator } from '../helpers/string-validation-helper'
 
 
-
 describe('SignUp controller', () => {
     test('should return 400 if no name is provided', () => {
         const sut = new SignUpController()
@@ -35,20 +34,4 @@ describe('SignUp controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('email'))
     }) 
-    
-    test('should return false if no valid email is provided', () => {
-        const sut = new StringValidator()
-        const email = "email"
-
-        const validation = sut.validateEmail(email)
-        expect(validation).toBe(false)
-    })
-
-    test('should return true if no valid email is provided', () => {
-        const sut = new StringValidator()
-        const email = "email@gmail.com.br"
-
-        const validation = sut.validateEmail(email)
-        expect(validation).toBe(true)
-    })
 })
---
+++ src/presentation/helpers/string.validation-helper.spec.ts
@@ -0,0 +1,19 @@
+import { StringValidator } from './string-validation-helper'
+
+describe('StringValidator', () => {
+    test('should return false if no valid email is provided', () => {
+        const sut = new StringValidator()
+        const email = "email"
+    
+        const validation = sut.validateEmail(email)
+        expect(validation).toBe(false)
+    })
+    
+    test('should return true if no valid email is provided', () => {
+        const sut = new StringValidator()
+        const email = "email@gmail.com.br"
+    
+        const validation = sut.validateEmail(email)
+        expect(validation).toBe(true)
+    })
+})
