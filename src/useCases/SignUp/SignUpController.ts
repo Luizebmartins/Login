d646a34fa9b6003cc76c9fb0c45ebb5f6fa3b650
Luiz Martins <luizmartins.prog@gmail.com>
2022-10-03 19:47:02 -0300

    feat: ensure SignUpController return an error and 400 if no phone is provided

--- src/useCases/SignUp/SignUpController.ts
+++ src/useCases/SignUp/SignUpController.ts
@@ -11,7 +11,7 @@ export class SignUpController {
     ) {}
     
     handle(httpRequest: HttpRequest): HttpResponse {
-        const requiredFields = ['name', 'email', 'password', 'confirmPassword']
+        const requiredFields = ['name', 'email', 'password', 'confirmPassword', "phone"]
 
         for(const field of requiredFields) {
             if(!httpRequest.body[field]) {
