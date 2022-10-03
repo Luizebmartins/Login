02ce0637ec12fd94553c2416a25b65c1bd3ada7a
Luiz Martins <luizmartins.prog@gmail.com>
2022-10-03 19:47:11 -0300

    test: ensure SignUpController return an error and 400 if no phone is provided

--- src/useCases/SignUp/SignUpController.spec.ts
+++ src/useCases/SignUp/SignUpController.spec.ts
@@ -12,7 +12,8 @@ describe('SignUp controller', () => {
             body: {
                 email: 'email@email.com',
                 password: 'any',
-                confirmPassword: 'any'
+                confirmPassword: 'any',
+                phone: "35997464533"
             }
         }
 
@@ -28,7 +29,8 @@ describe('SignUp controller', () => {
             body: {
                 name: 'anyName',
                 password: 'any',
-                confirmPassword: 'any'
+                confirmPassword: 'any',
+                phone: "35997464533"
             }
         }
 
@@ -45,7 +47,8 @@ describe('SignUp controller', () => {
                 name: 'anyName',
                 email: 'email@.com',
                 password: 'any',
-                confirmPassword: 'any'
+                confirmPassword: 'any',
+                phone: "35997464533"
             }
         }
 
@@ -60,7 +63,8 @@ describe('SignUp controller', () => {
             body: {
                 name: 'anyName',
                 email: 'email@teste.com',
-                confirmPassword: 'any'
+                confirmPassword: 'any',
+                phone: "35997464533"
             }
         }
 
@@ -76,6 +80,7 @@ describe('SignUp controller', () => {
                 name: 'anyName',
                 email: 'email@teste.com',
                 password: 'any',
+                phone: "35997464533"
             }
         }
 
@@ -83,4 +88,22 @@ describe('SignUp controller', () => {
         expect(httpResponse.statusCode).toBe(400)
         expect(httpResponse.body).toEqual(new MissingParamError('confirmPassword'))
     }) 
+
+    test('should return 400 if no phone is provided', () => {
+        const sut = new SignUpController(emailValidator)
+        const httpRequest = {
+            body: {
+                name: 'anyName',
+                email: 'email@teste.com',
+                password: 'any',
+                confirmPassword: 'any',
+            
+            }
+        }
+
+        const httpResponse = sut.handle(httpRequest)
+        expect(httpResponse.statusCode).toBe(400)
+        expect(httpResponse.body).toEqual(new MissingParamError('phone'))
+    }) 
+
 })
