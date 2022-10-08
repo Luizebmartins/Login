c1eb36df651881229962274cc5f0a59b01831e2d
Luiz Martins <luizmartins.prog@gmail.com>
2022-10-08 13:34:46 -0300

    refactor: att SignUpDTO

--- src/useCases/SignUp/SignUpDTO.ts
+++ src/useCases/SignUp/SignUpDTO.ts
@@ -1,5 +1,15 @@
+interface Address {
+  cep: string,
+  number: string,
+  street: string
+}
+
+
 export interface ICreateUserRequestDTO {
     name: string;
     email: string;
     password: string;
-  }
+    phone?: Array<string>,
+    score?: number,
+    address?: Array<Address>
+}
