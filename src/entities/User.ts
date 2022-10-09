ecf07b59e75fcca810241046c6a339c62f850667
Luiz Martins <luizmartins.prog@gmail.com>
2022-10-09 14:08:11 -0300

    feat: att User entity

--- src/entities/User.ts
+++ src/entities/User.ts
@@ -5,15 +5,22 @@ interface Address {
 }
   
 export class User {
-    public name!: string;
-    public email!: string;
+    public name!: string
+    public email!: string
     public password!: string
     public phone?: Array<string>
     public score?: number
     public address?: Array<Address>
+    public t_admin?: boolean
+    public t_adop?: boolean
+    public t_volunt?: boolean
 
     constructor(props: User) {
         Object.assign(this, props)
+        this.score = props.score || 0
+        this.t_admin = props.t_admin || false
+        this.t_adop = props.t_adop || false
+        this.t_volunt = props.t_volunt || false
     }
 
 }
