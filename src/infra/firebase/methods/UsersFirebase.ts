fc382c80eed7e2e93ac5917a607262aa7b90b25c
Luiz Martins <luizmartins.prog@gmail.com>
2022-10-22 20:39:28 -0300

    refactor: console.log removal

--- src/infra/firebase/methods/UsersFirebase.ts
+++ src/infra/firebase/methods/UsersFirebase.ts
@@ -11,7 +11,6 @@ export class UsersFirebase implements IUsersDbaMethods {
     async get(email: string):Promise<any> {
         const userSnapshot = (await this.usersRef.where('email', '==', email).get()).docs
         if(!userSnapshot.length) {
-            console.log("não existe")
             return false
         }
         const userData = userSnapshot[0].data()
@@ -23,7 +22,6 @@ export class UsersFirebase implements IUsersDbaMethods {
             await this.usersRef.add({...user})
             return true 
         } catch (error) {
-            console.log(error)
             return false
         }
     }
