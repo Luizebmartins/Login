048947183935ffb0935b0c0b0c8ee74589dc5643
Luiz Martins <luizmartins.prog@gmail.com>
2022-10-31 14:43:26 -0300

    feat: ensure SignInUseCase returns an error if user does not exist

--- src/useCases/SignIn/SignInUseCase.ts
+++ src/useCases/SignIn/SignInUseCase.ts
@@ -1,5 +1,13 @@
+import { IUsersRepository } from '../../repositories/IUsersRepository'
+import { User } from '../../entities/User'
+import * as bcrypt from 'bcrypt'
 export class SignInUseCase {
-    execute() {
-        return true
+    constructor(
+        private usersRepository: IUsersRepository
+    ) {}
+
+    execute(data: any) {
+        const user = this.usersRepository.get(data.email)
+        if(!user) throw new Error("Email or password is incorrect!")
     }
 }
