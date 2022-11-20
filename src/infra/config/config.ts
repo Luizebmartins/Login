b9249e5ee4a99392a69dff84b21a5b73922ee1a3
Luiz Martins <luizmartins.prog@gmail.com>
2022-11-20 19:57:49 -0300

    feat: add postgre connection using sequelize

---
+++ src/infra/config/config.ts
@@ -0,0 +1,28 @@
+require('dotenv').config()
+
+module.exports = {
+  development: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABSE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres'
+  },
+  test: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABSE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres'
+  },
+  production: {
+    username: process.env.PGUSER,
+    password: process.env.PGPASSWORD,
+    database: process.env.PGDATABSE,
+    host: process.env.PGHOST,
+    port: process.env.PGPORT,
+    dialect: 'postgres'
+  }
+}
