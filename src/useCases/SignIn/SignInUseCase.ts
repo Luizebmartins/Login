86cc80af2679789bc92f84fa19ebba71bdfa804d
Luiz Martins <luizmartins.prog@gmail.com>
2022-11-26 19:31:49 -0300

    feat: ensure SigInUseCase return true if the data matches

--- src/useCases/SignIn/SignInUseCase.ts
+++ src/useCases/SignIn/SignInUseCase.ts
@@ -1,15 +1,20 @@
 import { IUsersRepository } from '../../repositories/IUsersRepository'
 import * as bcrypt from 'bcrypt'
+
 export class SignInUseCase {
     constructor(
         private usersRepository: IUsersRepository
     ) {}
 
-    async execute(data: any) {
+    async execute(data: any): Promise<any> {
         const existingUser = await this.usersRepository.get(data.email)
         if(!existingUser) throw new Error("Email or password is incorrect!")
 
         const passwordMatch = await bcrypt.compare(data.password, existingUser.password)
         if(!passwordMatch) throw new Error("Email or password is incorrect!")
+        
+        return {
+            success: true,
+        }
     }
 }
