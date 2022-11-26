feb644f4b4c066990dac24adbc5aa6590839768e
Luiz Martins <luizmartins.prog@gmail.com>
2022-11-26 20:04:00 -0300

    refactor: change firebase to postgres in usersRepository inject in SignUpUseCase

--- src/useCases/SignUp/index.ts
+++ src/useCases/SignUp/index.ts
@@ -1,19 +1,13 @@
 import { SignUpController } from './SignUpController'
 import { SignUpUseCase } from './SignUpUseCase'
 import { UsersRepository } from '../../repositories/Implementations/UsersRepository'
-import { UsersFirebase } from '../../infra/firebase/methods/UsersFirebase'
-import { firebaseAdmin } from '../../infra/firebase/connection/firebase'
+import { UserPostgres } from '../../data/user/userData'
 import { StringValidator } from '../utils/helpers/string-validation-helper'
 
-const usersRef = firebaseAdmin.firestore().collection('users');
-const usersFirebase = new UsersFirebase(usersRef)
-
-
-const usersRepository = new UsersRepository(usersFirebase)
-
-const signUpUseCase = new SignUpUseCase(usersRepository)
+const usersRepository = new UsersRepository(new UserPostgres())
 const stringValidator = new StringValidator()
 
+const signUpUseCase = new SignUpUseCase(usersRepository)
 const signUpController = new SignUpController(stringValidator, signUpUseCase)
 
 export {signUpController, signUpUseCase}
