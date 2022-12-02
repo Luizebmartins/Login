db97336f38f9e97c1e61d6542d0da04df307a0e3
Luiz Martins <luizmartins.prog@gmail.com>
2022-12-01 21:05:44 -0300

    refactor: remove firebase connection

--- src/infra/firebase/connection/firebase.ts
+++
@@ -1,8 +0,0 @@
-import * as admin from 'firebase-admin';
-import * as serviceAccount from './firebase_dev.json';
-
-const firebaseAdmin = admin.initializeApp({
-   credential: admin.credential.cert(serviceAccount as admin.ServiceAccount)
-});
-
-export {firebaseAdmin}
