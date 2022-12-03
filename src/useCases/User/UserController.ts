6255da61120b6bf80ba1b7c0c5da9992e521da35
Luiz Martins <luizmartins.prog@gmail.com>
2022-12-03 14:25:27 -0300

    feat: ensure UserUseCase return true and status 200 if user is updated

--- src/useCases/User/UserController.ts
+++ src/useCases/User/UserController.ts
@@ -6,25 +6,39 @@ import { MissingTokenError } from '../utils/errors/missing-token-error'
 import { UnauthorizedError } from '../utils/errors/unauthorized-error'
 
 export class UserController {
+    constructor(
+        private userUseCase: any,
+    ) {}
+
     update(request: HttpRequest): HttpResponse {
-        if(!request.authentication) {
-            return unauthorized(new MissingTokenError())
-        }
-        if(request.authentication.id !== request.params.id) {
-            return forbidden(new UnauthorizedError())
-        }
-        if(!request.body) {
-            return badRequest(new MissingBodyError())
+        try {
+            if(!request.authentication) {
+                return unauthorized(new MissingTokenError())
+            }
+            if(request.authentication.id !== request.params.id) {
+                return forbidden(new UnauthorizedError())
+            }
+            if(!request.body) {
+                return badRequest(new MissingBodyError())
+            }
+
+            if(!request.body.password) {
+                return badRequest(new MissingParamError('password'))
+            }
+
+            const update = this.userUseCase.update(request.body)
+            if(update.success) {
+                return {
+                    statusCode: 200,
+                    body: {
+                        success: true
+                    }
+                }
+            }
+
+        } catch (error) {
+            
         }
 
-        if(!request.body.password) {
-            return badRequest(new MissingParamError('password'))
-        }
-
-
-        return {
-            statusCode: 200,
-            body: {}
-        }
     }
 }
